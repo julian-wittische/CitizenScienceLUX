f6e386420d966bf8649457afc723a69dce131555
julian-wittische <jwittische@gmail.com>
2025-07-28 15:47:10 +0200

    eregregr

--- 4_User.R
+++ 4_User.R
@@ -5,12 +5,52 @@
 # Data: MNHNL
 # Script objective : Analyses of the user focus section
 
+############ Local configuration ----
+source("config.R")
+
 ############ Load data ----
-load("iNat.RData") # Created using 1_Data.R
+#load(paste0(DATAPATH,"iNat.RData")) # Created using 1_Data.R
 
 ###### All observations from Luxembourg users
 
-all <- load("ALL_OBS.RData")
+# load(paste0(DATAPATH,"all_obs/ALL_OBS.RData"))
+# 
+# col_sizes <- sapply(ALL_OBS, object.size)
+# col_sizes_mb <- sort(col_sizes / (1024^2), decreasing = TRUE)
+# print(round(col_sizes_mb, 2))
+# print(round(col_sizes_mb, 2)/sum(col_sizes_mb)*100)
+
+# Find all observation objects 
+lf <- list.files(path = paste0(DATAPATH,"all_obs/"), pattern = "^ALL_OBS", full.names = TRUE)
+# Initialize list
+obj_ls <- vector("list", length(lf)) 
+# Loop to go through each and remove useless but sizable columns
+for (i in 1:length(lf)){
+  print(lf[i])
+  load(lf[i])
+  obj_ls[[i]] <- ALL_OBS[ , !(names(ALL_OBS) %in% c("identifications", "non_owner_ids",
+                                                    "observation_photos", "photos", "annotations",
+                                                    "project_observations", "outlinks",
+                                                    "comments", "votes", "faves"))]
+  rm(ALL_OBS)
+  gc()
+}
+
+lapply(obj_ls, dim)
+
+keep_common_columns <- function(df_list) {
+  # Find common column names across all list elements
+  common_cols <- Reduce(intersect, lapply(df_list, colnames))
+  
+  # Subset each data frame to those common columns
+  lapply(df_list, function(df) df[, common_cols, drop = FALSE])
+}
+
+obj_ls <- keep_common_columns(obj_ls)
+
+all <- do.call(rbind, obj_ls)
+
+save(all, file=paste0(DATAPATH,"all_obs/all.RData"))
 
 ############ Local configuration ----
 source("config.R")
@@ -33,6 +73,28 @@ source("ResidentVisitor.R")
 ###### For locals (residents), define their "territory" in terms of size and 
 
 ############ User specialization ----
+
+### Taxonomic
+
+# How many users have more than 100
+
+length(table(all$user.id))
+sum(table(all$user.id)>100)
+hist(table(all$user.id), breaks=3000, xlim=c(0,1500))
+
+library(inflection)
+user_obs_sorted <- log(sort(table(all$user.id)))
+names(user_obs_sorted) <- NULL
+infl <- inflection::uik(1:length(user_obs_sorted), user_obs_sorted)
+threshold <- user_obs_sorted[infl]
+exp(threshold)
+
+hist(user_obs_sorted, type = "l", main = "Sorted User Observation Counts")
+abline(v = threshold, col = "red", lty = 2)
+
+sum(table(all$user.id)>75)
+
+
 
 
 
